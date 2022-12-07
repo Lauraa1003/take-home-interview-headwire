d9933414272b118c9a669a1ef8c81aa9ff35aeea
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-12-07 14:17:20 +0100

    finished sidenav

---
+++ headwire-interview/src/components/SideNav/SideNav.js
@@ -0,0 +1,16 @@
+import './SideNav.scss'
+
+
+
+const SideNav = () => {
+  return (
+    <div class="side-nav">
+        <a href="https://www.instagram.com/">INSTAGRAM</a>
+        <a href="https://www.facebook.com/">FACEBOOK</a>
+        <a href="https://twitter.com/">TWITTER</a>
+        <a href="https://www.pinterest.com/">PINTEREST</a>
+    </div>
+  );
+}
+
+export default SideNav;
