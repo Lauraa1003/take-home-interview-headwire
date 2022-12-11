265a5010912251516ca54497e81284636ab82738
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-12-11 14:08:09 +0100

    created data file for services  section

---
+++ headwire-interview/src/components/Services/ServiceData.js
@@ -0,0 +1,42 @@
+import iconbox1 from '../../assets/imgs/iconbox1.1.png';
+import iconbox2 from '../../assets/imgs/iconbox1.2.png';
+import iconbox3 from '../../assets/imgs/iconbox1.3.png';
+
+const servicesData = [
+    {
+        title:'Design & Planning',
+        description:'We will help you to get the result you dreamed of.',
+        img: iconbox2,
+    },
+    {
+        title:'Custom Solutions',
+        description:'Individual, aesthetically stunning solutions for customers.',
+        img: iconbox1,
+    },
+    {
+        title:'Furniture & Decor',
+        description:'We create and produce our product design lines.',
+        img: iconbox3,
+    },
+];
+
+ const companyData = [
+    {
+        name: 'current clients',
+        value: '[ 180+ ]',
+    },
+    {
+        name: 'years of experience',
+        value: '[ 10+ ]',
+    },
+    {
+        name: 'awards winning',
+        value: '[ 35+ ]',
+    },
+    {
+        name: 'offices worldwide',
+        value: '[ 5+ ]',
+    },
+];
+
+export { servicesData, companyData };
