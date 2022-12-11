b283ec81695e8b347543d867b8d5c4be7d30f123
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-12-11 14:04:53 +0100

    created data file for blog

---
+++ headwire-interview/src/components/Blog/BlogData.js
@@ -0,0 +1,29 @@
+import grid1 from '../../assets/imgs/grid1.jpeg';
+import grid2 from '../../assets/imgs/grid2.jpeg';
+import grid3 from '../../assets/imgs/grid3.jpeg';
+
+const BlogData = [
+    {
+      image: grid1,
+      date: 'MARCH 19, 2020',
+      writer: 'TOM BLACK',
+      title: 'Top 10 Tips for Your Kitchen Interior Design',
+      description: 'A faceting effect livens up and...',
+    },
+    {
+      image: grid2,
+      date: 'MARCH 19, 2020',
+      writer: 'TOM BLACK',
+      title: 'The Golden Ratio Rules for Best 2D Sketch',
+      description: 'A faceting effect livens up and...',
+    },
+    {
+      image: grid3,
+      date: 'MARCH 19, 2020',
+      writer: 'TOM BLACK',
+      title: 'Use Pastel Colors & Natural Materials',
+      description: 'A faceting effect livens up and...',
+    },
+  ];
+
+export default BlogData;
