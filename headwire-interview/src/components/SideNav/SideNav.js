9a7f1d87d59f2448eacf70b95ee5a6cd349df0e5
Lauraa1003 <88458945+Lauraa1003@users.noreply.github.com>
2022-12-11 14:07:04 +0100

    formatted code

--- headwire-interview/src/components/SideNav/SideNav.js
+++ headwire-interview/src/components/SideNav/SideNav.js
@@ -1,16 +1,14 @@
-import './SideNav.scss'
-
-
+import "./SideNav.scss";
 
 const SideNav = () => {
   return (
     <div class="side-nav">
-        <a href="https://www.instagram.com/">INSTAGRAM</a>
-        <a href="https://www.facebook.com/">FACEBOOK</a>
-        <a href="https://twitter.com/">TWITTER</a>
-        <a href="https://www.pinterest.com/">PINTEREST</a>
+      <a href="https://www.instagram.com/">INSTAGRAM</a>
+      <a href="https://www.facebook.com/">FACEBOOK</a>
+      <a href="https://twitter.com/">TWITTER</a>
+      <a href="https://www.pinterest.com/">PINTEREST</a>
     </div>
   );
-}
+};
 
 export default SideNav;
--- headwire-interview/src/pages/Home/Home.js
+++ headwire-interview/src/pages/Home/Home.js
@@ -1,20 +1,26 @@
-import './Home.scss'
-import Navbar from '../../components/Navbar/Navbar';
-import HomeCarousel from '../../components/HomeCarousel/HomeCarousel';
-import Sidenav from '../../components/SideNav/SideNav';
-import SideNav from '../../components/SideNav/SideNav';
-import Quote from '../../components/Quote/Quote';
+import "./Home.scss";
+import Navbar from "../../components/Navbar/Navbar";
+import HomeCarousel from "../../components/HomeCarousel/HomeCarousel";
+import SideNav from "../../components/SideNav/SideNav";
+import Quote from "../../components/Quote/Quote";
+import Services from "../../components/Services/Services";
+import Professionals from "../../components/Professionals/Professionals";
+import Blog from "../../components/Blog/Blog";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
-
-    return (
-        <div className='home-wrap'>
-        <Navbar/>
-        <HomeCarousel/>
-        <SideNav/>
-        <Quote/>
-        </div>
-    );
-}
+  return (
+    <div className="home-wrap">
+      <Navbar />
+      <HomeCarousel />
+      <SideNav />
+      <Quote />
+      <Services />
+      <Professionals />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
